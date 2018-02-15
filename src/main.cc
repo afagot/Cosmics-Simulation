dc1db123d08702aa90d3feb311b829838f4bdedc
alexis fagot <alexis.fagot@ugent.be>
2018-02-14 20:56:29 -0600

    [RPC-School-Mexico-2018] Update of README.md

--- src/main.cc
+++ src/main.cc
@@ -20,30 +20,7 @@ using namespace std;
 
 int main(){
     //Output CSV file
-    ofstream output("Results.csv",ios::out);
-
-    /** PROGRAM CODE **/
-    // Start with random seed
-    random_device rd;
-    Generator generator(rd());
-
-    // Get the thing started
-    generator.discard(100);
-
-    //Definition of the scintillators
-    Scintillator botScint, topScint;
-
-    //Definition of the RPC
-    RPC Rpc;
-    setRPC(Rpc);
-
-    //Definitions of the coordinates and of the cosmic direction
-    Point muonPos, RPCPos;
-    Direction direction;
-
-    //Set thresholds
-    float Scint_threshold = 0.;
-    float RPC_threshold = GASTHICKNESS;
+    ofstream output("Results.dat",ios::out);
 
     //Data storage table.
     unsigned int StripData[NPARTITIONS][NSTRIPS] = {{0}};
@@ -56,38 +33,23 @@ int main(){
     float Stat = (float)Nmuons;
     float progress = 0.;
 
+    /** PROGRAM CODE **/
+
+    //here write the code to declare the useful objects you will need
+
+    //...
+
     for(unsigned int n=0; n<Nmuons; n++){
-        muonPos = getRandomMuonPosition(generator,a_Z0-TRIGGER_WIDTH*cos(beta));
-        direction = getRandomDirection(generator);
 
-        setScint(botScint,muonPos,direction,0.,beta);
-        setScint(topScint,muonPos,direction,TRIGGER_OFFSET,beta);
+        //here write the needed calculation.
+        //for each muon, generate a point and directions,
+        //get the position in the trigger scintillators
+        //check if the hits are in the trigger
+        //check the coordinates in the RPC
+        //increment the right StripData cell by checking
+        //in which partition and which strip the hit is
 
-        bool in_trigger = isInBottomScintillator(botScint,beta) && isInTopScintillator(topScint,beta);
-
-        if (in_trigger){
-            RPCPos = getHitPositionRPC(muonPos,direction);
-
-            double topDist = getDistanceThroughTopScint(topScint,beta);
-            double botDist = getDistanceThroughBottomScint(botScint,beta);
-            double rpcDist = getDistanceThroughRPC(direction);
-
-            bool too_short = (topDist <= Scint_threshold) || (botDist <= Scint_threshold) || (rpcDist <= RPC_threshold);
-
-            if(!too_short){
-                if(isInTrapezoid(RPCPos, Rpc.corners)){
-                    for(unsigned int p = 0; p < NPARTITIONS; p++){
-                        if(isInTrapezoid(RPCPos,Rpc.part[p].corners)){
-                            for(unsigned int s = 0; s < NSTRIPS; s++){
-                                if(isInTrapezoid(RPCPos,Rpc.part[p].strips[s])){
-                                    StripData[p][s]++;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        //...
 
         //Print progress bar
         progress = float(n/Stat);
@@ -107,9 +69,8 @@ int main(){
     cout << endl;
 
     //Print number of hits per strip into output file
-    for(unsigned int p = 0; p < NPARTITIONS; p++)
-        for(unsigned int s = 0; s < NSTRIPS; s++)
-            output << getPartition(p) << '\t' << s+1 << '\t' << StripData[p][s] << endl;
+
+    //...
 
     output.close();
 
